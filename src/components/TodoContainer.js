3563d514660bf81e8bd065bf79cff7006da95dfd
KareemWilson <kareemmagdywilson@gmail.com>
2022-10-31 15:17:17 +0200

    Create Components Files

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TodoContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Helloo from kareem</h1>
+                <p>I am react parent component</p>
+            </div>
+        );
+    }
+}
+
+export default TodoContainer;
